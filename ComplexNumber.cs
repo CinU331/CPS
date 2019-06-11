08ff1d2705e06919d3fabf7b6b7e640b8087c4a9
CinU331 <210246@edu.p.lodz.pl>
2019-06-11 15:36:29 +0200

    Added class for complex numbers and Fourier transformation

---
+++ ComplexNumber.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CPS
+{
+    class ComplexNumber
+    {
+        private double realPart;
+        private double imaginaryPart;
+
+        public ComplexNumber(){}
+
+        public ComplexNumber(double realPart, double imaginaryPart)
+        {
+            this.realPart = realPart;
+            this.imaginaryPart = imaginaryPart;
+        }
+
+        public static ComplexNumber ConvertFromPolarToRectangular(double r, double radians)
+        {
+            ComplexNumber data = new ComplexNumber(r * Math.Cos(radians), r * Math.Sin(radians));
+            return data;
+        }
+
+        public static ComplexNumber operator +(ComplexNumber x, ComplexNumber y)
+        {
+            ComplexNumber data = new ComplexNumber(x.realPart + y.realPart, x.imaginaryPart + y.imaginaryPart);
+            return data;
+        }
+        public static ComplexNumber operator -(ComplexNumber x, ComplexNumber y)
+        {
+            ComplexNumber data = new ComplexNumber(x.realPart - y.realPart, x.imaginaryPart - y.imaginaryPart);
+            return data;
+        }
+        public static ComplexNumber operator *(ComplexNumber x, ComplexNumber y)
+        {
+            ComplexNumber data = new ComplexNumber((x.realPart * y.realPart) - (x.imaginaryPart * y.imaginaryPart),
+                (x.realPart * y.imaginaryPart + (x.imaginaryPart + y.realPart)));
+            return data;
+        }
+
+        public double Magnitude
+        {
+            get
+            {
+                return Math.Sqrt(Math.Pow(realPart, 2) + Math.Pow(imaginaryPart, 2));
+            }
+        }
+
+        public double Phase
+        {
+            get
+            {
+                return Math.Atan(imaginaryPart / realPart);
+            }
+        }
+    }
+}
---
+++ Fourier.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace CPS
+{
+    class Fourier
+    {
+        public static ComplexNumber[] DFT(ComplexNumber[] complexNumbers)
+        {
+            int N = complexNumbers.Length;
+            ComplexNumber[] complexNumbers2 = new ComplexNumber[N];
+
+            for (int k = 0; k < N; k++)
+            {
+                complexNumbers2[k] = new ComplexNumber(0, 0);
+                for (int n = 0; n < N; n++)
+                {
+                    ComplexNumber tmp = ComplexNumber.ConvertFromPolarToRectangular(1, -2 * Math.PI * n * k / N);
+                    tmp *= complexNumbers[n];
+                    complexNumbers2[k] += tmp;
+                }
+            }
+            return complexNumbers2;
+        }
+
+        public static ComplexNumber[] FFT(ComplexNumber[] complexNumbers)
+        {
+            int N = complexNumbers.Length;
+            ComplexNumber[] complexNumbers2 = new ComplexNumber[N];
+            ComplexNumber[] x, X, y, Y;
+
+            if (N == 1)
+            {
+                complexNumbers2[0] = complexNumbers[0];
+                return complexNumbers2;
+            }
+
+            int k;
+            x = new ComplexNumber[N / 2];
+            y = new ComplexNumber[N / 2];
+            for (k = 0; k < N / 2; k++)
+            {
+                x[k] = complexNumbers[2 * k];
+                y[k] = complexNumbers[2 * k + 1];
+            }
+
+            X = FFT(x);
+            Y = FFT(y);
+
+            for (k = 0; k < N / 2; k++)
+            {
+                ComplexNumber tmp = ComplexNumber.ConvertFromPolarToRectangular(1, -2 * Math.PI * k / N);
+                X[k] *= tmp;
+            }
+            for (k = 0; k < N / 2; k++)
+            {
+                complexNumbers2[k] = X[k] + Y[k];
+                complexNumbers2[k + N / 2] = Y[k] - X[k];
+            }
+            return complexNumbers2;
+        }
+    }
+}
